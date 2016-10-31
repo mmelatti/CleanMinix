7ffdeca00798e409d1f0d48de2dbbd3769913cad
mmelatti <melatti.michael@gmail.com>
2016-10-31 12:45:37 +0000

    Server is working now and recieving messages

---
+++ servers/ss/proto.h
@@ -0,0 +1,26 @@
+#ifndef _SS_PROTO_H
+#define _SS_PROTO_H
+
+/* Function prototypes. */
+
+/* main.c */
+int main(int argc, char **argv);
+
+/* store.c */
+int do_publish(message *m_ptr);
+int do_retrieve(message *m_ptr);
+int do_retrieve_label(const message *m_ptr);
+int do_subscribe(message *m_ptr);
+int do_check(message *m_ptr);
+int do_delete(message *m_ptr);
+int do_snapshot(message *m_ptr);
+int do_getsysinfo(const message *m_ptr);
+int sef_cb_init_fresh(int type, sef_init_info_t *info);
+
+/* Michael Melatti in main.c*/
+int do_sema_init(message *m_ptr)
+{
+	printf("Michael server call\n");
+	return(OK);
+}
+#endif
